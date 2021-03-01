cdac6046203bc91882122d317e5cb3c503e6c681
MarcioFujita <hanker78@gmail.com>
2021-03-01 18:51:19 -0300

    Ex1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "",
+      loading: true,
+    };
+    this.loadDog = this.loadDog.bind(this);
+  }
 
-    </div>
-  );
-}
+  componentDidMount() {
+    this.loadDog();
+  }
+
+
+  loadDog() {
+    fetch("https://dog.ceo/api;breeds/image/random")
+      .them(res => res.json())
+      .them(result => this.setState({ data: result, loading: false }));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loading && <h1>LOADING...</h1>}
+        <button onClick={this.loadDog}>New Dog!</button>
+        <div>
+          <img src={this.state.data.message} alt="Random dog" />
+        </div>
+      </div>
+    );
+  }
+}  
 
 export default App;
