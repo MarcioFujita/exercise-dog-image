a3826bafc540fda91666f1f07ebdfde499682136
MarcioFujita <hanker78@gmail.com>
2021-03-01 19:25:17 -0300

    Ex1 V2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: "",
-      loading: true,
+      data: ""
     };
     this.loadDog = this.loadDog.bind(this);
   }
@@ -17,15 +16,16 @@ class App extends React.Component {
 
 
   loadDog() {
-    fetch("https://dog.ceo/api;breeds/image/random")
-      .them(res => res.json())
-      .them(result => this.setState({ data: result, loading: false }));
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(result => this.setState({ data: result }));
   }
 
   render() {
+    if (this.state.data === "") return "loading...";
     return (
       <div>
-        {this.state.loading && <h1>LOADING...</h1>}
+        <p>Dogs!</p>
         <button onClick={this.loadDog}>New Dog!</button>
         <div>
           <img src={this.state.data.message} alt="Random dog" />
